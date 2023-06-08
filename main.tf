b9099dd6d3a29bfc7bf70f47f6b640b26baa4353
ivan.voloboyev <vano3231@gmail.com>
2023-06-09 02:22:07 +0300

    Updae main.tf config

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ module "github_repository" {
   github_token             = var.GITHUB_TOKEN
   repository_name          = var.FLUX_GITHUB_REPO
   public_key_openssh       = module.tls_private_key.public_key_openssh
-  public_key_openssh_title = "flux"
+  public_key_openssh_title = "flux0"
 }
 
 module "kind_cluster" {
@@ -20,13 +20,11 @@ module "kind_cluster" {
 }
 
 module "flux_bootstrap" {
-  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap?ref=gke_auth"
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
   github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
   private_key       = module.tls_private_key.private_key_pem
   github_token      = var.GITHUB_TOKEN
-  config_host       = module.gke_cluster.config_host
-  config_token      = module.gke_cluster.config_token
-  config_ca         = module.gke_cluster.config_ca
+  config_path       = module.kind_cluster.kubeconfig
 }
 
 module "tls_private_key" {
